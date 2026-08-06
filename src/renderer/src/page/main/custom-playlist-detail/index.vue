d7588c24bb4b9dabc26899e915fe0698e8edbc57
coder-zhx <coder_zhx@163.com>
2026-08-06 14:49:18 +0800

    私人歌单支持下载等操作

--- src/renderer/src/page/main/custom-playlist-detail/index.vue
+++ src/renderer/src/page/main/custom-playlist-detail/index.vue
@@ -22,39 +22,7 @@ const userStore = useUserStore()
 const playlistId = route.params.id as string
 
 const playlist = ref()
-const playState = playService.state
 const list = ref<Song[]>([])
-const columns = [
-  {
-    title: '歌曲/歌手',
-    key: 'name',
-    ellipsis: true,
-  },
-  {
-    title: '专辑',
-    key: 'al',
-    ellipsis: true,
-  },
-  {
-    title: '时长',
-    key: 'dt',
-    width: '100px',
-  },
-  {
-    title: '操作',
-    key: 'op',
-    width: '80px',
-    align: 'center',
-  },
-]
-
-const customRow = (record) => {
-  return {
-    onClick: () => {
-      playService.playSong(record)
-    },
-  }
-}
 
 const canEdit = computed(() => {
   if (!playlist.value) return false
@@ -206,46 +174,13 @@ function handleEdit() {
         </div>
       </div>
 
-      <a-table :columns="columns" :data-source="list" :pagination="false" :custom-row="customRow">
-        <template #bodyCell="{ column, record }">
-          <div v-if="column.key === 'name'" class="name">
-            <div class="pic-wrap">
-              <img :src="$imgSize(record.al.picUrl, 40, 40)" alt="" />
-            </div>
-            <div class="info">
-              <div class="song-name">
-                <span class="ellipsis">{{ record.name }}</span>
-                <PlayingIcon
-                  v-if="record.id === playState.curSong?.id"
-                  :is-playing="playState.isPlaying"
-                  style="font-size: 16px"
-                ></PlayingIcon>
-              </div>
-              <div class="artist">
-                <template v-for="(artist, index) in record.ar" :key="index">
-                  <span>{{ artist.name }} </span>
-                  <i v-if="index !== record.ar.length - 1">/</i>
-                </template>
-              </div>
-            </div>
-          </div>
-          <template v-if="column.key === 'op'">
-            <div class="op" @click.stop>
-              <a @click="deleteRow(record)">
-                <Iconfont name="icon-delete"></Iconfont>
-              </a>
-            </div>
-          </template>
-          <template v-if="column.key === 'al'">{{ record.al.name }}</template>
-          <template v-if="column.key === 'dt'">{{ $duration(record.dt) }}</template>
+      <SongTable :list="list">
+        <template #opBtns="{ record }">
+          <a @click="deleteRow(record)">
+            <Iconfont name="icon-delete"></Iconfont>
+          </a>
         </template>
-        <template #emptyText>
-          <div class="no-data">
-            <Iconfont name="icon-no-data"></Iconfont>
-            <div>没有歌曲，去收藏一些吧~</div>
-          </div>
-        </template>
-      </a-table>
+      </SongTable>
     </div>
   </div>
 </template>
@@ -298,74 +233,6 @@ function handleEdit() {
         color: $primary;
       }
     }
-  }
-}
-
-.name {
-  display: flex;
-  gap: 4px;
-
-  .pic-wrap {
-    width: 40px;
-    height: 40px;
-    border-radius: 4px;
-    overflow: hidden;
-
-    img {
-      width: 100%;
-      height: 100%;
-    }
-  }
-
-  .info {
-    flex: 1;
-    width: 0;
-
-    div {
-      text-overflow: ellipsis;
-      overflow: hidden;
-      white-space: nowrap;
-    }
-
-    .song-name {
-      display: flex;
-      align-items: center;
-      gap: 10px;
-    }
-
-    .artist {
-      font-size: 12px;
-      color: $text-light;
-      text-overflow: ellipsis;
-      overflow: hidden;
-      white-space: nowrap;
-    }
-  }
-}
-
-.op {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 10px;
-  font-size: 18px;
-
-  a {
-    display: flex;
-  }
-}
-
-.no-data {
-  height: 60vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 40px;
-  color: $text-light;
-
-  .icon-no-data {
-    font-size: 100px;
   }
 }
 </style>
